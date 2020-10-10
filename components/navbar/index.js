0324cfca17edcc96b68f902075afdb93b994ce0f
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-10 12:50:03 -0400

    buttons on the navBar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,1 +1,15 @@
 //this is the component that new game, high scores, and sign out tab
+
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <button class="newGame">New Game</button>
+            <button class="highScores">High Scores</button>
+            <button Class="signOut">Sign Out</button>
+        </div>
+    )
+}
+
+export default Navbar;
