5db6ed532241af50d0612f175d78c569925f7509
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-10 13:11:53 -0400

    achievedwordsstarter

--- components/achievedWords/index .js
+++ components/achievedWords/index .js
@@ -1,1 +1,11 @@
 // this is where the achieved words are displayed
+
+import React from "react";
+
+function AchievedWords() {
+    return (
+        //render the words that we have gotten correctly here
+    )
+}
+
+export default AchievedWords;
