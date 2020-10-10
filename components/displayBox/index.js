888731f0a125802cdd1517d43ff37ae18c08b25f
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-10 13:35:41 -0400

    displaybox

--- components/displayBox/index.js
+++ components/displayBox/index.js
@@ -1,1 +1,11 @@
 //this is the file that displays the word for kids to catch
+
+import React from "react";
+
+function DisplayBox() {
+    return (
+        //code for display box
+    )
+}
+
+export default DisplayBox;
