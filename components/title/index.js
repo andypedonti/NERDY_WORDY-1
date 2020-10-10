5a63efc80988db07463f834a666a6440b9bc75f3
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-10 11:57:54 -0400

    title component js

--- components/title/index.js
+++ components/title/index.js
@@ -1,1 +1,13 @@
 //just renders the "nerdy wordy title"
+
+import React from "react";
+
+function Title() {
+    return (
+        <div className="title">
+            <h1 className="titleText">NERDY WORD</h1>
+        </div>
+    )
+}
+
+export default Header;
