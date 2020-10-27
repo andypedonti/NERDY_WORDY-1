a19f79a380f1553a92aebd07556c88aa233d587a
Unko <kobie.watkins@gmail.com>
2020-10-27 00:04:09 -0400

    edited the wording in the about statement

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -10,7 +10,7 @@ const About = () => (
                 <h1>Nerdy_Wordy Team</h1>
                 <p>
                     The Nerdy_Wordy Team is a cohort from the Chapel Hill UNC Coding Bootcamp.
-                    This team is created and developed an Toddler - Elementary school game that promotes visual and kinesthetic
+                    Our team created a Toddler - Elementary school game, that promotes visual and kinesthetic
                     learning development.
                     In this first draft and inital concept we have the React-dnd utilites and
                     letters cascading downward for the user to drag and drop.
